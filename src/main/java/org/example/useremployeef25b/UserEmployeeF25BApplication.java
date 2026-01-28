f7ee4c63c7799b54d8174e13654abf1b135f3761
erlmek <erlm@ek.dk>
2026-01-28 09:22:25 +0100

    Initial commit

---
+++ src/main/java/org/example/useremployeef25b/UserEmployeeF25BApplication.java
@@ -0,0 +1,13 @@
+package org.example.useremployeef25b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserEmployeeF25BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserEmployeeF25BApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/useremployeef25b/UserEmployeeF25BApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.useremployeef25b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserEmployeeF25BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
